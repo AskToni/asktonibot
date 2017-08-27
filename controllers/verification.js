a2a50344a36af93945b942ae376ac4ffe130ac79
Edison Yang <edison.cy.yang@gmail.com>
2017-08-26 23:51:38 -0700

    Basic asktoni welcome message reply

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,10 +2,10 @@ module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
 
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query[hub.verify_token] === 'ask toni is cool');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'ask toni is cool');
 
     if(hubMode && verifyTokenMatches) {
-        res.statis(200).send(hubChallenge);
+        res.status(200).send(hubChallenge);
     }
     else {
         res.status(403).end();
